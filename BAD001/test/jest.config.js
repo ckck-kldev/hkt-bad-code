9cb9a5c9a73e830473190479491ea34e13aa8423
ckck.kldev <ckck.kldev@gmail.com>
2023-04-13 12:31:20 +0800

    factorial, fibonacci test added

--- BAD001/test/jest.config.js
+++ BAD001/test/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  // testMatch: ["**/ex-*.test.ts"],
+  testMatch: ["**/ex-*.test.ts"],
   verbose: true,
   // failFast: true,
 };
--- BAD001/test/test/ex-1.test.ts
+++ BAD001/test/test/ex-1.test.ts
@@ -1,5 +1,18 @@
 import { test, expect } from "@jest/globals";
+import { factorial, fibonacci } from "../code/factorial_fibonacci";
+// import { beforeEach } from "node:test";
 
 test("this test will not run", () => {
   expect("A").toBe("A");
+});
+
+test("factorial functions", () => {
+  // beforeEach(() => {
+  //   let num = 0;
+  // });
+
+  test("example of !3 = 3 * 2 * 1)", () => {
+    let num = 3;
+    expect(factorial(num)).toEqual(6);
+  });
 });
