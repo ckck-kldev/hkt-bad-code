7a602f8351a40acc73137efdbd0acf521343b8ad
ckck.kldev <ckck.kldev@gmail.com>
2023-04-17 01:57:10 +0800

    commit

--- BAD003/lecture-demo/migrations/20230416124915_normalize-product-type.ts
+++ BAD003/lecture-demo/migrations/20230416124915_normalize-product-type.ts
@@ -6,10 +6,15 @@ export async function up(knex: Knex): Promise<void> {
     table.string("name", 30).unique().notNullable();
   });
 
-  let rows = await knex("product").distinct("type");
+  let rows = await knex("product").distinct("type as name");
+  console.log(rows);
+
   for (const row of rows) {
-    await knex("product_type").insert({ name: row.type });
+    let [{ id }] = await knex("product_type")
+      .insert({ name: row.name })
+      .returning("id");
   }
+  throw new Error("stop");
 }
 
 export async function down(knex: Knex): Promise<void> {
